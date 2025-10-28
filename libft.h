79bb61bd28d406b95ee02249d2667bc0596c6ae3
El Mehdi Boulajdad <eboulajd@e2r7p9.1337.ma>
2025-10-28 08:41:58 +0100

    last test

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eboulajd <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:13:49 by eboulajd          #+#    #+#             */
-/*   Updated: 2025/10/27 17:36:34 by eboulajd         ###   ########.fr       */
+/*   Updated: 2025/10/28 08:38:22 by eboulajd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,12 @@ size_t	ft_strlcpy(char *dst, const char *srs, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int		ft_atoi(const char *nptr);
 char	*ft_strchr(const char *s, int c);
 void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
